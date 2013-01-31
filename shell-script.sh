35fd054ab117a8456aaba1f31e5b9a28e7866409
Peter Gabrielsen <peterg@peterg-tp.(none)>
2013-01-31 15:14:07 +0100

    Added the shell script plus a java class that can execute the shell script with whatever argument are given it. Only runs on linux.

---
+++ shell-script.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+
+echo "<html>";
+echo "<body>";
+echo "<h1>dDist 2011 - Example Page 4</h1>";
+echo "<p>This is what was sent to the shell script:" $1 " </p>";
+echo "</body>";
+echo "</html>";
+
