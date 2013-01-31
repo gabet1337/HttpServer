87906bec0fb8503638e7a0264be75e305edd3783
Peter Gabrielsen <peterg@cs.au.dk>
2013-01-31 14:02:13 +0100

    Formatter for folders added. It formats a folder structure as html

---
+++ src/services/FolderParser.java
@@ -0,0 +1,28 @@
+package services;
+
+import java.net.*;
+import java.io.*;
+
+public class FolderParser {
+
+    private FolderBuilder builder;
+    private File folder;
+
+    public FolderParser(FolderBuilder builder, URI path) {
+        this.builder = builder;
+        this.folder = new File(path);
+    }
+    
+    public String parse() {
+        for (File f : folder.listFiles()) {
+            if (f.isDirectory()) {
+                builder.parseFolder(f);
+            } else if (f.isFile()) {
+                builder.parseFile(f);
+            } else {
+                //Do nothing if it isn't a file or folder
+            }
+        }
+        return builder.getResult();
+    }
+}
